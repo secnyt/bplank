5d7d01e0ce2506bf2d3b94f84ec51a4845639002
secnyt <47509759+secnyt@users.noreply.github.com>
2022-04-21 15:18:38 +0000

    Sticky navbar and main homepage body

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 import NavBar from '../src/components/navbar/navbar'
+import DummyText from '../src/components/dummytext'
 
 import styles from '../styles/Home.module.css'
 
@@ -17,6 +18,28 @@ export default function Home() {
 
       <main>
         <NavBar />
+
+        <div className={styles.main}>
+          <div>
+            <div className={styles.homebigtext}>
+              Blogs. <br/>
+              Refurbished. <br/>
+              Reclaimed.
+            </div>
+            <small className={styles.small}>
+              (Also open-source!)
+            </small>
+          </div>
+          
+          <p>
+            We turned the log into a plank.
+            <small className={styles.small + ' ' + styles.getit}>
+              (Get it?!)
+            </small>
+          </p>
+          <DummyText />
+        </div>
+        
       </main>
     </div>
   )
---
+++ src/components/dummytext.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default class DummyText extends React.Component {
+  constructor (props) {
+    super(props)
+    this.props = props
+  }
+
+  render () {
+    return (
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean bibendum scelerisque lectus sed venenatis. Curabitur luctus libero a ante tristique aliquet. Donec sollicitudin, nisi sed scelerisque auctor, risus sapien viverra velit, at tristique mi nibh in quam. Cras nec feugiat sem, vel rutrum erat. Vivamus justo felis, vulputate vel blandit ac, aliquet vel sapien. Etiam non tortor cursus, interdum erat feugiat, porttitor nisl. Fusce vulputate maximus ullamcorper. Praesent sem sapien, varius id porta quis, pulvinar blandit magna. Nulla facilisi.
+        </p>
+        <p>
+          Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam cursus in sapien eget iaculis. Maecenas consequat purus ut interdum porta. Aenean faucibus lacinia porta. Aliquam ultricies vulputate turpis, eget ullamcorper dolor lobortis ut. Curabitur et augue et lorem tempor iaculis. Suspendisse potenti.
+        </p>
+        <p>
+          Quisque sed urna quis nibh luctus laoreet eget ac lectus. Suspendisse eu imperdiet ligula. In at sem non nulla ultricies finibus at id arcu. Pellentesque neque diam, faucibus at tincidunt id, imperdiet sed arcu. Aenean eros arcu, molestie sed rutrum suscipit, interdum non dolor. Integer tellus ligula, finibus in elementum ullamcorper, semper in purus. Duis gravida justo nisi, sed maximus ex rhoncus a. Maecenas vel finibus tortor, mattis tincidunt dolor. Donec pulvinar, purus vel sagittis vehicula, justo nisl aliquam urna, vitae vehicula urna ante vitae neque. Donec consectetur, odio eget lacinia elementum, sem libero egestas magna, sed varius lectus justo ut risus. Etiam venenatis, massa eget lobortis pretium, dui dui vehicula lacus, nec porta ligula orci vitae nisl. Maecenas bibendum eget diam vitae dictum. Quisque eu magna ut nisl porta auctor. In hac habitasse platea dictumst. Suspendisse sit amet libero dictum, blandit turpis a, volutpat urna. Donec in metus id sem sodales elementum.
+        </p>
+        <p>
+            Duis consequat ultricies nulla vel semper. Morbi aliquam vulputate sapien, in malesuada elit dignissim vel. Praesent sit amet est a arcu convallis pulvinar a ac magna. Ut lectus lacus, sodales in vulputate in, fringilla a lacus. Sed a efficitur neque. Donec maximus enim nisi, mattis vehicula leo condimentum nec. Duis vestibulum egestas mi, id elementum quam accumsan vel. Donec lobortis dui in odio molestie mollis. Vivamus ac diam orci. In hac habitasse platea dictumst. Nunc quam mauris, fermentum ut mi eget, elementum feugiat arcu. Aliquam placerat ex nisi, at efficitur risus ullamcorper eget.
+        </p>
+        <p>
+            In sed mauris vitae justo malesuada rutrum. Cras tristique euismod odio, vitae lacinia urna molestie vel. Curabitur viverra sem non orci rutrum consequat. Fusce nec viverra mi. Maecenas scelerisque ut justo et lacinia. Morbi laoreet ante sit amet blandit eleifend. Curabitur quis ipsum consequat, mattis lacus vitae, volutpat velit. Duis laoreet sodales lacus. Maecenas euismod aliquet blandit. Praesent eget odio neque. Vestibulum maximus quam fermentum ornare gravida. Sed id urna in magna sollicitudin porttitor.
+        </p>
+      </div>
+    )
+  }
+}
